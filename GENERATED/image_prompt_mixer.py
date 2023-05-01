2e1a9e0b788560ac6d4b9356adbde0fc76ca6321
ian287913 <45512766+ian287913@users.noreply.github.com>
2023-05-01 14:22:45 +0800

    feat: add noise on image prompt

---
+++ GENERATED/image_prompt_mixer.py
@@ -0,0 +1,42 @@
+import sys
+import os
+import requests
+import asyncio
+import random
+import io
+import base64
+import json
+import time
+from io import BytesIO
+from PIL import Image
+from time import gmtime, strftime
+import parameters
+
+
+# image route (relative)
+IMAGE_FOLDER = '/images/'
+
+def randomize_image_color(image_name):
+    # load prompt image from png to base64 format
+    img = Image.open(f'.\{image_name}')
+    img = img.convert("RGB")
+    pixels = img.load() # create the pixel map
+    for i in range(img.size[0]): # for every pixel:
+        for j in range(img.size[1]):
+            if pixels[i,j] != (0, 0, 0):
+                pixels[i,j] = (int(random.random()*256), int(random.random()*256), int(random.random()*256))
+    img.save(f'.\(randomized){image_name}')
+    print(f'randomized image saved to .\(randomized){image_name}')
+
+if __name__ == '__main__':
+    start_time = time.time()
+    
+    print(f'len(sys.argv) = {len(sys.argv)}')
+    if (len(sys.argv) >= 2):
+        randomize_image_color(sys.argv[1])
+    else:
+        print('expect argv[1] = image name')
+
+    end_time = time.time()
+    time_lapsed = end_time - start_time
+    print(f'Total time elapsed: {time_lapsed}s')
--- GENERATED/parameters.py
+++ GENERATED/parameters.py
@@ -1,18 +1,20 @@
 PARAMETERS = {
     'sd_model_checkpoint' : ['v1-5-pruned.ckpt [e1441589a6]'],
     'image_prompt' : 'image prompt gray.png',
-    'prompt_species' : ['regal_blue_tang', 'bowfin fish', 'koi', 'queen angelfish', 'striped bass', 'croaker', 'tuna'],
+    ##'image_prompt' : 'image prompt random.png',
+    'prompt_species' : ['regal blue tang', 'bowfin fish', 'koi', 'queen angelfish', 'striped bass', 'croaker', 'tuna', 'goldfish'],
+    ##'prompt_species' : ['regal blue tang'],
     'prompt' : "A detailed 3d character design of one (#SPECIES#) character in the style of pixar, black background, side view, detailed fish scale, concept artwork by pixar, character design by pixar, 8k, uhd, high definition, unreal 5, daz, octane render", 
 
     'negative_prompt' : "((duplicate)) ((culled)) (((out of frame))) (((deformed))) ((bad anatomy)) ((extra eyes)) ((without head)) ((disfigured))",
 
-    'cfg_scale' : [11],
+    'cfg_scale' : [10, 11, 12],
     'denoising_strength' : [0.80],
     'steps' : [100],
     # generate total batch_width**2 images
     'batch_width' : 3,
     'seed' : 1,
-    "sampler_index": "Euler a",
+    "sampler_index": "Euler",
     'include_init_images' : True
     #sampler_name
     #mask_blur = 4
