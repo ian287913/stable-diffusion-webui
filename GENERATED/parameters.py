70eb5b481d024c77c11a0560389e20e5887b19af
ian287913 <45512766+ian287913@users.noreply.github.com>
2023-06-18 21:36:25 +0800

    final exp

--- GENERATED/parameters.py
+++ GENERATED/parameters.py
@@ -5,22 +5,19 @@ TEXT2IMAGE_PARAMETERS = {
     'image_prompt' : 'image prompt gray.png',
     ##'image_prompt' : 'image prompt random.png',
     ##'prompt_species' : ['regal blue tang', 'bowfin fish', 'koi', 'queen angelfish', 'striped bass', 'croaker', 'tuna', 'goldfish'],
-    'prompt_species' : ['goldfish'],
+    'prompt_species' : ['regal blue tang', 'bowfin fish', 'koi', 'queen angelfish', 'striped bass', 'croaker', 'tuna'],
     'prompt' : "A detailed 3d character design of one (#SPECIES#) character in the style of pixar, black background, side view, detailed fish scale, concept artwork by pixar, character design by pixar, 8k, uhd, high definition, unreal 5, daz, octane render", 
 
-    'negative_prompt' : "((duplicate)) ((culled)) (((out of frame))) (((deformed))) ((bad anatomy)) ((extra eyes)) ((without head)) ((disfigured))",
+    'negative_prompt' : "((disfigured)) ((extra eyes)) ((duplicate)) ((culled)) (((out of frame))) (((deformed))) ((bad anatomy)) ((without head))",
 
-    'cfg_scale' : [10, 11, 12],
-    'denoising_strength' : [0.75, 0.80, 0.85],
+    'cfg_scale' : [11],
+    'denoising_strength' : [0.80, 0.85],
     'steps' : [100],
     # generate total batch_width**2 images
-    'batch_width' : 6,
+    'batch_width' : 10,
     'seed' : 1,
     "sampler_index": "Euler a",
     'include_init_images' : True
-    #sampler_name
-    #mask_blur = 4
-    #"include_init_images": false,
 }
 
 # this is for INPAINTING
